059238605f191f2f342bd5f9163231da74f6ce89
kenphanith <33407061+phanithken@users.noreply.github.com>
2023-06-28 15:31:15 +0900

    Delete provision.sh

--- provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/arch/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# install node@14
-brew install node@14
